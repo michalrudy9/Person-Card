407c3be1f565a73518504db153fba27b005c9fdd
michalrudy9 <michallrudy9@gmail.com>
2023-12-10 15:14:32 +0100

    Extracted view to subview

--- PersonCard/ContentView.swift
+++ PersonCard/ContentView.swift
@@ -12,14 +12,43 @@ struct ContentView: View {
         ZStack {
             Color(red: 0.09, green: 0.63, blue: 0.52)
                 .edgesIgnoringSafeArea(.all)
-            Text("John Smith")
-                .font(Font.custom("Pacifico-Regular", size: 40))
-                .bold()
-                .foregroundStyle(.white)
+            VStack {
+                Image("sampleUser")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                Text("John Smith")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundStyle(.white)
+                Text("iOS Developer")
+                    .foregroundStyle(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+44 123 456 789", imageName: "phone.fill")
+            }
         }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct InfoView: View {
+    let text: String
+    let imageName: String
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25.0)
+            .fill(Color.white)
+            .frame(height: 50)
+            .padding(.all)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundStyle(Color(red: 0.09, green: 0.63, blue: 0.52))
+                Text(text)
+            })
+    }
 }
