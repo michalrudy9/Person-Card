5d08a3e8c61cdca0bb52b1f1ed9f9e8d8cdb6760
michalrudy9 <michallrudy9@gmail.com>
2023-12-10 14:22:37 +0100

    Prepared background for app

--- PersonCard/ContentView.swift
+++ PersonCard/ContentView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(.all)
+            Text("John Smith")
+                .font(.title)
+                .bold()
+                .foregroundStyle(.white)
+        }
     }
 }
 
