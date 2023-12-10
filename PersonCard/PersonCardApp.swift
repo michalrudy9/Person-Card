36650ff3a913810c457febe1bd26e5f0a89149d8
michalrudy9 <michallrudy9@gmail.com>
2023-12-10 14:03:04 +0100

    Initial Commit

---
+++ PersonCard/PersonCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCardApp.swift
+//  PersonCard
+//
+//  Created by Michał Rudy on 10/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct PersonCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
